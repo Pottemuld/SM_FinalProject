358380a000cb329642db34316c60502a9a6f8b05
Pottemuld <32219655+Pottemuld@users.noreply.github.com>
2021-05-22 18:25:07 +0200

    pics of graphs for bestk

--- FinalProject.R
+++ FinalProject.R
@@ -91,17 +91,20 @@ print( sum(diag(cf$table))/sum(cf$table) )
 
 
 #k experiments (train = 90% id_full  test = 10% id_full)
-k <- round(sqrt(nrow(id_full_shuf)), digits = 0)
+#k <- round(sqrt(nrow(id_full_shuf)), digits = 0)
 train_bestk <- id_full_reduced[1:(nrow(id_full_reduced)/10 *9),]
 test_bestk <- id_full_reduced[((nrow(id_full_reduced)/10 *9) + 1) : nrow(id_full_reduced),]
 
-train_labels <- id_full_shuf[1:(nrow(id_full_reduced)/10 *9),]
-test_labels <- id_full_shuf[((nrow(id_full_reduced)/10 *9) + 1) : nrow(id_full_reduced),]
+train_labels <- id_full_shuf[1:(nrow(id_full_reduced)/10 *9), 1]
+test_labels <- id_full_shuf[((nrow(id_full_reduced)/10 *9) + 1) : nrow(id_full_reduced),1]
 
-values <- seq.int(min(k/4), max(k*4), length.out= 50)
+#values <- seq.int(min(1), max(200), length.out= 100)
+values <- c(3, 77, 167, 203, 277, 327, 415)
 result <-c(1:length(values))
 kvalues <- c(1:length(values))
 for(i in 1:length(values)){
+  print(i)
+  print(length(values))
   
   test_pred <- knn(train = train_bestk, test = test_bestk, cl = train_labels, k=values[i])
   
